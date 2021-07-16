ef93a05b85e410de03b4d5ce2f3eda90749d2dc2
Francisco Kahil <codexico@gmail.com>
2021-07-16 14:10:34 +0300

    upd build

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    candidate: null,
-    candidate_position: null,
+    start_cand: null,
+    end_cand: null,
+    start_position: null,
+    end_position: null,
+
     routeqs: {},
     dataLoading: true,
     fileName:null,
@@ -18,13 +21,18 @@ export default new Vuex.Store({
   mutations: {
     SET_ROUTEQS: (state, obj) => {
       state.routeqs = obj;
-      const { candidate, file } = obj;
-      state.candidate = parseInt(candidate)
+      const { start, end, file } = obj;
+      state.start_cand=start;
+      state.end_cand=end;
        state.fileName= file
     },
-    SET_CANDIDATE_POSITION: (state, ind) => {
+    SET_START_POSITION: (state,  ind) => {
 
-      state.candidate_position = ind
+      state.start_position= ind
+    },
+    SET_END_POSITION: (state,  ind) => {
+
+      state.end_position= ind
     },
     DATA_LOADING: (state) => {
       // state.dataLoaded = false;
@@ -52,8 +60,13 @@ export default new Vuex.Store({
       });
 
       commit('SET_DATA', data)
-      const candidateIndex = _.findIndex(data, (i) => (i.x == state.candidate))
-      commit("SET_CANDIDATE_POSITION", candidateIndex)
+      const startIndex = _.findIndex(data, (i) => (i.x == state.start_cand))
+      
+      const endIndex = _.findIndex(data, (i) => (i.x == state.end_cand))
+      commit("SET_START_POSITION",  startIndex)
+      commit("SET_END_POSITION",  endIndex)
+      console.debug("START IND", startIndex)
+      console.debug("END POS", endIndex)
       commit('DATA_LOADED')
     }
   },
